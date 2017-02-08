ac0dcdd5d7195873b4a85b66aba04d4686589203
Ashish Singh <imrealashu@gmail.com>
2017-02-09 00:18:44 +0530

    `route.js` file updated and server port chaned.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
-  app.get('/', function(req, response){
+  app.get('/', function(req, res){
     res.send('Welcome to Personal Finance')
   })
 }
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 require('./routes/api')(app)
 
-app.listen(3000, function(){
-  console.log('listening on 3000')
+app.listen(8000, function(){
+  console.log('listening on 8000')
 })

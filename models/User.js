ea92c2ef00e131ee1396abee4ee81ea5b52d176c
Ashish Singh <imrealashu@gmail.com>
2017-02-14 23:01:03 +0530

    User model added with basic schema.

---
+++ models/User.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: Boolean,
+  location: String,
+  meta: {
+    age: Number,
+    website: String
+  },
+  created_at: Date,
+  updated_at: Date
+});
+
+userSchema.methods.dudify = function() {
+  // add some stuff to the users name
+  this.name = this.name + '-dude';
+
+  return this.name;
+}
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+// make this available to our users in our Node applications
+module.exports = User;

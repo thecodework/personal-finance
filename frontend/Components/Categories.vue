3104ab4285491a20b8c5c18938d739699ba485e6
Ashish Singh <imrealashu@gmail.com>
2017-02-20 09:53:50 +0530

    Categories Component - Addtion to the previous commit, this file wasn't committed accidently.

---
+++ frontend/Components/Categories.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+    <h1>Welcome To Example Component</h1>
+    <h2>Manage Categories</h2>
+    <div>
+        <input type="text" name="category-name" v-model="categoryName">
+        <button type="button" @click="addCategory" v-if="! isUpdating">Add Category</button>
+        <button type="button" @click="updateCategory" v-else >Update Category</button>
+    </div>
+    <div>
+        <ul>
+            <li v-for="category in categories">{{ category.name }} <button style="color: green" @click="editCategory(category._id)">Edit</button> <button style="color: red" v-on:click="deleteCategory(category._id)">Delete</button></li>
+        </ul>
+    </div>
+</div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      categoryName: '',
+      categories: [],
+      isUpdating: false,
+      updatingCategoryId: ''
+    }
+  },
+  methods: {
+    addCategory () {
+        axios.post('http://localhost:8000/api/category', {categoryName: this.categoryName}).then(function(response){
+            this.fetchCategory()
+            this.categoryName = ''
+        }.bind(this))
+    },
+    fetchCategory () {
+        axios.get('http://localhost:8000/api/category').then(function(response){
+            this.categories = response.data
+        }.bind(this))
+    },
+    editCategory (id) {
+        this.isUpdating = true
+        axios.get('http://localhost:8000/api/category/' + id).then(function(response){
+            this.categoryName = response.data.name
+            this.updatingCategoryId = response.data._id
+        }.bind(this))
+    },
+    updateCategory () {
+        axios.patch('http://localhost:8000/api/category/',{id: this.updatingCategoryId, categoryName: this.categoryName}).then(function(response){
+            this.fetchCategory()
+            this.categoryName = ''
+            this.isUpdating = false
+        }.bind(this))
+    },
+    deleteCategory (id) {
+        axios.delete('http://localhost:8000/api/category/' + id).then(function(response){
+            this.fetchCategory()
+        }.bind(this))
+    }
+  },
+  mounted () {
+    this.fetchCategory()
+  }
+}
+</script>

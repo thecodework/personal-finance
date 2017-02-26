6f1d6b78fad478f655a4f66a42bcc165cf1ef809
Ashish Singh <imrealashu@gmail.com>
2017-02-26 15:00:10 +0530

    Test - Test cases added for Category API

--- test/category.js
+++ test/category.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = 'test';
 let mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
 let Category = require('./../models/Category')
 let chai = require('chai');
 let chaiHttp = require('chai-http');
@@ -24,7 +25,7 @@ describe('Category', () => {
     });
 
     describe('/POST /api/category', () => {
-        it('it should create category', (done) => {
+        it('it should create a category', (done) => {
             let category = {
                 name: 'Transport',
                 type: 'expense'
@@ -35,6 +36,68 @@ describe('Category', () => {
                     res.should.have.status(200)
                     res.body.should.be.a('object')
                 done()
+            })
+        })
+    });
+
+    describe('/GET/:id /api/category', () => {
+        it('it should get a category by id', (done) => {
+            let category = new Category({
+                name: 'Transport',
+                type: 'expense'
+            })
+            category.save((err, category) => {
+                chai.request(app)
+                    .get('/api/category/' + category.id)
+                    .send(category)
+                    .end((err, res)=> {
+
+                    res.should.have.status(200);
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('name')
+                    res.body.should.have.property('type')
+                    res.body.should.have.property('_id').eql(category.id)
+
+                    done()
+                })
+            })
+        })
+    });
+
+    describe('/PATCH/:id /api/category', () => {
+        it('it should update a category', (done) => {
+            let category = new Category({
+                name: 'Transports',
+                type: 'income'
+            })
+            category.save((err, category) => {
+                chai.request(app)
+                    .patch('/api/category/')
+                    .send({id: category.id, name: 'Transport', type: 'expense'})
+                    .end((err, res) => {
+                        res.should.have.status(200)
+                        res.body.should.be.a('object')
+                        done()
+                })
+            })
+        })
+    })
+
+    describe('/DELETE/:id /api/category', () => {
+        it('it should delete a category by id', (done) => {
+            let category = new Category({
+                name: 'Transport',
+                type: 'expense'
+            })
+            category.save((err, category) => {
+                chai.request(app)
+                    .delete('/api/category/' + category.id)
+                    .send(category)
+                    .end((err, res)=> {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object')
+                    done()
+                })
             })
         })
     });
